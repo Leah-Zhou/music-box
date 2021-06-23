beb35e0c080f378e447a0d55e9e145f189192262
leahchow <ctzhouleya@gmail.com>
2021-06-23 12:29:23 -0400

    update-pkjson

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,5 @@
 
 
-
-// const path = require('path');
-
-// module.exports = {
-//   entry: './src/js/index.js',
-//   output: {
-//     path: path.resolve(__dirname, 'dist/assets'),
-//     filename: 'bundle.js', 
-//   },
-//   devServer: {
-//     contentBase: path.resolve(__dirname, 'dist'),
-//     publicPath: '/assets/',
-//     open:true
-//   },
-//   module: {
-//     rules: [{
-//       test: /\.js$/,
-//       exclude: /node_modules/,
-//       use: {
-//         loader: 'babel-loader',
-//         options: {
-//           presets: ['@babel/preset-env']
-//         }
-//       }
-//     },
-//     {
-//       test:/\.s[ac]ss$/i,
-//       use:['style-loader', 'css-loader', 'sass-loader'],
-//     }
-//   ]
-//   }
-// };
-
-
-
 const path = require('path');
 
 module.exports={
